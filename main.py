92b22b6f7101b9fb5fc28c11cb2e341a5a5a64f4
Carsten S <3000dilandau@gmail.com>
2025-06-15 02:23:48 -0700

    currently working on recognizing OLLs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 import kociemba
 import random
 import pycuber as pc
@@ -6,6 +7,82 @@ import pycuber as pc
 kociemba_map = {"w":"U", "r":"R", "g":"F", "y":"D", "o":"L", "b":"B"}
 moves = ["U","U'","U2","R","R'","R2","F","F'","F2","D","D'","D2","L","L'","L2","B","B'","B2"]
 wide_moves = {"R":"M'", "L":"M", "U":"E'", "D":"E", "F":"S", "B":"S'"}
+
+upper_face_edges = {
+    (0,1): ("B", 0,1),
+    (1,0): ("L", 0,1),
+    (1,2): ("R", 0,1),
+    (2,1): ("F", 0,1),
+} #UB, UL, UR, UF
+
+upper_face_corners = {
+    (0,0): [("B", 0,2), ("L", 0,0)],
+    (0,2): [("B", 0,0), ("R", 0,2)],
+    (2,0): [("F", 0,0), ("L", 0,2)],
+    (2,2): [("F", 0,2), ("R", 0,0)],
+} #UBL, UBR, UFL, UFR
+
+oll_algs = [
+    "R U2 R2 F R F' U2 R' F R F'",
+    "R U' R2 D' r U r' D R2 U R'",
+    "f R U R' U' f' U' F R U R' U' F'",
+    "f R U R' U' f' U F R U R' U' F'",
+    "r' U2 R U R' U r",
+    "r U2 R' U' R U' r'",
+    "r U R' U R U2 r'",
+    "R' F' r U' r' F2 R",
+    "R U R' U' R' F R2 U R' U' F'",
+    "R U R' U R' F R F' R U2 R'",
+    "r' R2 U R' U R U2 R' U M'",
+    "M' R' U' R U' R' U2 R U' M",
+    "F U R U2 R' U' R U R' F'",
+    "r U R' U' r' F R2 U R' U' F'",
+    "r' U' r R' U' R U r' U r",
+    "R' F R U R' U' F' R U' R' U2 R",
+    "F R' F' R U S' R U' R' S",
+    "R U2 R2 F R F' U2 M' U R U' r'",
+    "S' R U R' S U' R' F R F'",
+    "S' R U R' S U' M' U R U' r'",
+    "R U2 R' U' R U R' U' R U' R'",
+    "R U2 R2 U' R2 U' R2 U2 R",
+    "R2 D R' U2 R D' R' U2 R'",
+    "r U R' U' r' F R F'",
+    "F' r U R' U' r' F R",
+    "R U2 R' U' R U' R'",
+    "R U R' U R U2 R'",
+    "r U R' U' M U R U' R'",
+    "r2 D' r U r' D r2 U' r' U' r",
+    "F U R U2 R' U' R U2 R' U' F'",
+    "R' U' F U R U' R' F' R",
+    "S R U R' U' R' F R f'",
+    "R U R' U' R' F R F'",
+    "R U R2 U' R' F R U R U' F'",
+    "R U2 R2 F R F' R U2 R'",
+    "R U2 r D r' U2 r D' r' R'",
+    "F R' F' R U R U' R'",
+    "R U R' U R U' R' U' R' F R F'",
+    "f' r U r' U' r' F r S",
+    "R' F R U R' U' F' U R",
+    "R U R' U R U2 R' F R U R' U' F'",
+    "R' U' R U' R' U2 R F R U R' U' F'",
+    "f' L' U' L U f",
+    "f R U R' U' f'",
+    "F R U R' U' F'",
+    "R' U' R' F R F' U R",
+    "F R' F' R U2 R U' R' U R U2 R'",
+    "F R U R' U' R U R' U' F'",
+    "r U' r2 U r2 U r2 U' r",
+    "r' U r2 U' r2 U' r2 U r'",
+    "f R U R' U' R U R' U' f'",
+    "R' F' U' F U' R U R' U R",
+    "r' U2 R U R' U' R U R' U r",
+    "r U R' U R U' R' U R U2 r'",
+    " R' F U R U' R2 F' R2 U R' U' R",
+    "r U r' U R U' R' U R U' R' r U' r'",
+    "R U R' U' M' U R U' r'"
+]
+
+oll_alglist = {num: alg for num, alg in list(enumerate(oll_algs, start=1))}
 
 def fix_wide_moves(alg: str):
     move_list = []
@@ -58,26 +135,56 @@ def cube_solver_prompt():
     face_string = ''.join(face_list)
     return ''.join(kociemba_map[c] for c in face_string)
 
-def get_valid_oll(cube, face_key="U"):
+def get_oll_ss(cube, face_key="U"):
     face = cube.get_face(face_key)
     center_color = face[1][1].colour
 
-    edges = [(0,1),(1,0),(1,2),(2,1)]
-    corners = [(0,0),(2,0),(0,2),(2,2)]
+    # check edges
+    edge_codes = []
+    for (i,j), (side, side_i, side_j) in upper_face_edges.items():
+        if face[i][j].colour == center_color:
+            edge_codes.append(0)
+        else:
+            alt_color = cube.get_face(side)[side_i][side_j].colour
+            if alt_color == center_color:
+                edge_codes.append(1)
+            else:
+                raise ValueError("⚠️ invalid OLL case!")
 
-    check_edges = [1 if face[i][j].colour == center_color else 0 for (i,j) in edges]
-    check_corners = [1 if face[i][j].colour == center_color else 0 for (i,j) in corners]
+    # check corners
+    corner_codes = []
+    for (i,j), side_list in upper_face_corners.items():
+        if face[i][j].colour == center_color:
+            corner_codes.append(0)
+        else:
+            corner_code = -1
+            for code, (side, side_i, side_j) in enumerate(side_list, start=1):
+                if cube.get_face(side)[side_i][side_j].colour == center_color:
+                    corner_code = code
+                    break
+            if corner_code < 0:
+                raise ValueError("⚠️ invalid OLL case!")
+            corner_codes.append(corner_code)
 
-    if sum(check_edges) in (1,3):
-        return None
+    if sum(edge_codes) in (1,3):
+        raise ValueError("⚠️ invalid OLL case!")
 
-    return tuple(check_edges + check_corners)
+    return tuple(edge_codes + corner_codes)
+
+def get_oll_case(cube, face_key="U"):
+    possible_cases = [get_oll_ss(cube, face_key)]
+    for _ in range(3):
+        cube("U")
+        possible_cases.append(get_oll_ss(cube, face_key))
+
+    cube("U") #undo the setup moves
+    return min(possible_cases)
 
 def oll_trainer():
     cube = pc.Cube()
-    cube("r U R' U R U2 r'")
+    cube("L U L' U' L' B L B'")
     print(generate_scramble(min_len=5,max_len=15,mode=10))
-    print(get_valid_oll(cube, face_key="U"))
+    print(get_oll_case(cube, face_key="U"))
 
 def pll_trainer():
     print("n/a")
